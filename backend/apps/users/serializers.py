526ada4ee6270b129c2890d1f2843fb975ced8fe
charlesxie <408737515@qq.com>
2026-06-10 17:15:29 +0800

    管理员

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -10,8 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined')
-        read_only_fields = ('id', 'date_joined')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_superuser', 'date_joined')
+        read_only_fields = ('id', 'is_superuser', 'date_joined')
 
 
 class LoginSerializer(serializers.Serializer):
